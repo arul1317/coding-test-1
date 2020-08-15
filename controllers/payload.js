4914d2b54142eee2bf0940b417302585cbd02c18
roshni13 <roshni.smit@gmail.com>
2020-08-15 21:20:41 +0530

    adding prop declaration and block scope

--- controllers/payload.js
+++ controllers/payload.js
@@ -2,7 +2,7 @@ var data;
 var flag=0;
 
 const iterateObject = (obj) =>{
-    for(prop in obj){
+    for(let prop in obj){
         console.log("checking type of value in this object ")
         console.log(obj[prop]);
         if(obj[prop].valueType!="string"){
@@ -12,20 +12,21 @@ const iterateObject = (obj) =>{
         else{
             console.log("its a string");
             console.log(obj[prop].value);
-            var valuetobereplaced = obj[prop].value;
-            for(key in data.referenceData){
+            let valuetobereplaced = obj[prop].value;
+            for(const key in data.referenceData){
                 if(valuetobereplaced.indexOf(key) != -1){
                     flag =1;
-                    var r = "{" + key + "}";
-                    var newstr = valuetobereplaced.replace(r,data.referenceData[key])
+                    let r = "{" + key + "}";
+                    valuetobereplaced = valuetobereplaced.replace(r,data.referenceData[key])
                     console.log("its equal to prop" + key);
                     console.log("after replacing ")
-                    console.log(newstr);
+                    console.log(valuetobereplaced);
                 }
             }
             if(flag==1){
-             obj[prop].value = newstr;
+             obj[prop].value = valuetobereplaced;
              console.log(obj[prop].value);
+             flag=0;
             }
             
         }    
