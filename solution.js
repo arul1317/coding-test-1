aa513e4bdece6ffa5927f8852657f75dd5ddcbc6
Saravanan-M95 <68906539+Saravanan-M95@users.noreply.github.com>
2020-08-17 10:47:05 +0530

    Commit after Changes

---
+++ solution.js
@@ -0,0 +1,66 @@
+/*
+
+Use "http://localhost:3000/" POST method to add the JSON data and use "http://localhost:3000/" GET Method to substitute the {REF_*} values
+Tool : Postman
+*/
+
+var express = require('express');
+
+const replaceString = require('replace-string');
+
+var app = express();
+
+var out=[];
+
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json({ limit: "50mb" }))
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }))
+
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({extended : false}));
+
+app.get('/', function(req,res)
+{
+	res.send(out);
+});
+
+app.post('/', function(req,res)
+{
+	var input = req.body;
+
+	var ref=(input.referenceData);
+
+	var myJSON = JSON.stringify(input.payload);
+
+	var mapDict = new Map();
+	
+	for(x in ref)
+	{
+		mapDict.set(x,ref[x]);
+	}
+
+	var itr=mapDict.keys();
+
+	for(var a=0;a<mapDict.size;a++)
+	{
+		const key=itr.next().value;
+		console.log(key +'   '+mapDict.get(key));
+		myJSON = replaceString(myJSON, '{'+key+'}', mapDict.get(key));
+	}
+
+	input=JSON.parse(myJSON);
+	
+	if(input)
+	{
+		delete input['referenceData'];	
+		out.push(input);
+		res.status(200).send(input);
+	}
+});
+
+app.listen(3000, function()
+{
+	console.log('Server Starts');
+});
+
